020991029b0a7c6e3e050b1c8a2471096edb7514
Weronika Materna <wmaterna8@gmail.com>
2022-03-24 16:45:20 +0100

    setup react project with routes

---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface props {
+
+}
+
+const Dashboard: React.FC<props> = ({}) => {
+    return(
+        <div>
+            <h1>Dashboard yaaay</h1>
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface props {
+
+}
+
+const SignIn: React.FC<props> = ({}) => {
+    return(
+        <div>
+            <h1>SignIn yaaay</h1>
+        </div>
+    )
+}
+
+export default SignIn;
