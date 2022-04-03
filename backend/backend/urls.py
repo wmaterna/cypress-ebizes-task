ea8e72b9a5bbedce4ebdc4dd1ea376d91aa2f573
Stanislav Shevchuk <xahter.2000@gmail.com>
2022-04-03 19:59:51 +0200

    add urls.py in app:vetclinic

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,13 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from vetclinic import views
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('', views.frontpage_view),
     path('admin/', admin.site.urls),
-    path('login/', views.login_view),
-    path('login/members/', views.loggedin_view),
-    path('logout/', views.logout_view),
+    path('', include("vetclinic.urls")),
 ]
